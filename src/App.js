c30412e1b8a6e718b3fc3aa496596ae573bd33f7
Shivank-Wadke <shivankwadke@gmail.com>
2025-07-28 22:41:08 +0530

    learn adding dark mode to textutils

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Textform from './components/Textform';
+
+{/*import About from './components/About';*/}
 
 function App() {
   return (
     <>
     <Navbar title = "TextUtils" aboutText="About"/>
+    <div className="container my-3">
+      <Textform heading ="Enter the text to analyse"/>
+      {/*<About/>*/}
+    </div>
+    
     </>
   );
 }
---
+++ src/components/Textform.js
@@ -0,0 +1,42 @@
+import React,{useState} from 'react'
+
+export default function Textform(props) {
+    const [text, setText] = useState("");
+    const handleUpClick = () => {
+        let newText= text.toUpperCase();
+        setText(newText)
+    }
+    const handleLoClick = () => {
+        let newText= text.toLowerCase();
+        setText(newText)
+    }
+    const handleOnChange = (event) => {
+        setText(event.target.value);
+    }
+    const handleClr =() => {
+        let newText="";
+        setText(newText)
+    }
+    
+    return (
+        <>
+        <div className='container'>
+            <h1>{props.heading}</h1>
+            <div className="mb-3">
+            <textarea className="form-control" value={text} onChange={handleOnChange}id="exampleFormControlTextarea1" rows="8"></textarea>
+            </div>
+            <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to UpperCase</button>
+            <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to LowerCase</button>
+            <button className="btn btn-primary mx-1" onClick={handleClr}>ClearText</button>
+            
+        </div>
+        <div className="container my-3" >
+            <h2>Your Text Summary</h2>
+            <p> ~ {text.length} characters and {text.split(" ").length} words</p>
+            <p>~ {0.008*text.split(" ").length} Minutes read</p>
+            <h2>Preview</h2>
+            <p>{text}</p>
+        </div>
+        </>
+    )
+}
