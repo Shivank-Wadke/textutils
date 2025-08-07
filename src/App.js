a06014547e293b62eddfcf57396872c4e2f01709
Shivank-Wadke <shivankwadke@gmail.com>
2025-08-07 23:20:51 +0530

    Learn About Favicon Learnt about JS templates `...` enhanced use of states and props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,53 @@
+import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
+import Alert from './components/Alert';
 
 {/*import About from './components/About';*/}
 
 function App() {
+  const[mode,setMode] = useState('light')
+  const [text,setText] = useState('Enable Dark Mode')
+  const[toggleColor,setToggleColor] = useState('dark')
+  const[h1Color,setH1Color] = useState('black')
+  const [alert,setAlert] = useState(null)
+  const alertMessage = (message,type)=> {
+    setAlert({
+      msg:message,
+      type:type
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  }
+  const setToggle = () => {
+  if (mode === 'light') {
+    setMode('dark');
+    setText('Enable Light Mode');
+    setToggleColor('light');
+    setH1Color('white');
+    document.body.style.backgroundColor = '#212529';
+    alertMessage("Dark Mode has been enabled", "success");
+  } else {
+    setMode('light');
+    setText('Enable Dark Mode');
+    setToggleColor('dark');
+    setH1Color('black');
+    document.body.style.backgroundColor = '#ffffff';
+    alertMessage("Light Mode has been enabled", "success");
+  }
+};
+
+  
   return (
     <>
-    <Navbar title = "TextUtils" aboutText="About"/>
+    <Navbar title = "TextUtils" aboutText="About" mode={mode} toggle = {setToggle} text={text} toggleColor={toggleColor} />
+    <Alert alert={alert}/>
     <div className="container my-3">
-      <Textform heading ="Enter the text to analyse"/>
+      <Textform heading ="Enter the text to analyse" h1Color={h1Color} />
       {/*<About/>*/}
+      
     </div>
     
     </>
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -21,9 +21,9 @@ export default function Textform(props) {
     return (
         <>
         <div className='container'>
-            <h1>{props.heading}</h1>
+            <h1 style={{color:props.h1Color}}>{props.heading}</h1>
             <div className="mb-3">
-            <textarea className="form-control" value={text} onChange={handleOnChange}id="exampleFormControlTextarea1" rows="8"></textarea>
+            <textarea className="form-control" value={text} onChange={handleOnChange}id="exampleFormControlTextarea1" rows="8" ></textarea>
             </div>
             <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to UpperCase</button>
             <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to LowerCase</button>
@@ -31,11 +31,11 @@ export default function Textform(props) {
             
         </div>
         <div className="container my-3" >
-            <h2>Your Text Summary</h2>
-            <p> ~ {text.length} characters and {text.split(" ").length} words</p>
-            <p>~ {0.008*text.split(" ").length} Minutes read</p>
-            <h2>Preview</h2>
-            <p>{text}</p>
+            <h2 style={{color:props.h1Color}}>Your Text Summary</h2>
+            <p style={{color:props.h1Color}}> ~ {text.length} characters and {text.split(" ").length} words</p>
+            <p style={{color:props.h1Color}}>~ {0.008*text.split(" ").length} Minutes read</p>
+            <h2 style={{color:props.h1Color}}>Preview</h2>
+            <p style={{color:props.h1Color}}>{text}</p>
         </div>
         </>
     )
